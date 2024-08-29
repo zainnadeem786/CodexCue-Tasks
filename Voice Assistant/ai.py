da40794a5948b6a1b2e43e478d178e0019cb03d5
Zain Nadeem <165761530+zainnadeem786@users.noreply.github.com>
2024-08-29 17:35:17 +0500

    Add files via upload

---
+++ Voice Assistant/ai.py
@@ -0,0 +1,65 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+            return command
+    except:
+        print("Sorry, I didn't catch that. Please try again.")
+        return ""
+
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+    elif 'tell me ' in command:
+        person = command.replace('Tell me About', '')
+        person = ' '.join(word.capitalize() for word in person.split())
+        try:
+            info = wikipedia.summary(person, 5)
+            print(info)
+            talk(info)
+        except wikipedia.exceptions.PageError:
+            talk("Sorry, I couldn't find any information on " + person)
+    elif 'date' in command:
+        talk('sorry, I am Unavailible to go with you because i am Your Virtual Assistant')
+    elif 'are you single' in command:
+        talk('I am in a relationship with wifi')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    else:
+        talk('Please say the command again.')
+
+
+while True:
+    run_alexa()
